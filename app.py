6b944df9c1e83eaa2c675dded8836f2b30275aaf
arsaltauqeer <arsal.tauqeer@tecspine.com>
2024-02-25 02:05:08 +0500

    git eof error remove try

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from utilities.errors import errors
 load_dotenv()
 app = Flask(__name__)
 
+
 app.secret_key = os.getenv('APP_SECRET_KEY')
 app.config['SESSION_TYPE'] = 'filesystem'
 sess = Session()
